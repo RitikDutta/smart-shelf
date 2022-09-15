1e95f8ab038cb1bc955616b9b53c8cd456cb73ab
ritik dutta <ritikduttagd@gmail.com>
2022-09-15 16:58:06 +0530

    added key listner to arduino ino

--- arduino_async_webserver.ino
+++ arduino_async_webserver.ino
@@ -347,64 +347,51 @@ body {
     
   
   <script>
-      document.addEventListener('keydown', (event) => {
+    document.addEventListener('keydown', (event) => {
     var name = event.key;
     var code = event.code;
-
-
-
+    arr_check = []
 
       if (name == '1') {
-        checkbox = document.getElementsByName("13");
-        checkbox[0].checked = !checkbox[0].checked;
+        arr_check.push(13);
       }
       else if (name == '3') {
-        checkbox = document.getElementsByName("16");
-        checkbox[0].checked = !checkbox[0].checked;
+        arr_check.push(16);
       }
       else if (name == '7') {
-        checkbox = document.getElementsByName("1");
-        checkbox[0].checked = !checkbox[0].checked;
+        arr_check.push(1);
       }
       else if (name == '9') {
-        checkbox = document.getElementsByName("4");
-        checkbox[0].checked = !checkbox[0].checked;
+        arr_check.push(4);
       }
       else if (name == '2') {
-        checkbox = document.getElementsByName("14");
-        checkbox[0].checked = !checkbox[0].checked;
-        checkbox = document.getElementsByName("15");
-        checkbox[0].checked = !checkbox[0].checked;
+        arr_check.push(14, 15);
       }
       else if (name == '4') {
-        checkbox = document.getElementsByName("5");
-        checkbox[0].checked = !checkbox[0].checked;
-        checkbox = document.getElementsByName("9");
-        checkbox[0].checked = !checkbox[0].checked;
+        arr_check.push(5, 9);
       }
       else if (name == '6') {
-        checkbox = document.getElementsByName("8");
-        checkbox[0].checked = !checkbox[0].checked;
-        checkbox = document.getElementsByName("12");
-        checkbox[0].checked = !checkbox[0].checked;
+        arr_check.push(8, 12);
       }
       else if (name == '8') {
-        checkbox = document.getElementsByName("2");
-        checkbox[0].checked = !checkbox[0].checked;
-        checkbox = document.getElementsByName("3");
-        checkbox[0].checked = !checkbox[0].checked;
+        arr_check.push(2, 3);
       }
       else if (name == '5') {
-        checkbox = document.getElementsByName("6");
-        checkbox[0].checked = !checkbox[0].checked;
-        checkbox = document.getElementsByName("7");
-        checkbox[0].checked = !checkbox[0].checked;
-        checkbox = document.getElementsByName("10");
-        checkbox[0].checked = !checkbox[0].checked;
-        checkbox = document.getElementsByName("11");
-        checkbox[0].checked = !checkbox[0].checked;
+        arr_check.push(6, 7, 10, 11);       
       }
 
+      arr_check.forEach(myFunction);
+
+      function myFunction(value) {
+        checkbox = document.getElementsByName(value);
+        checkbox[0].checked = !checkbox[0].checked;
+        var xhr = new XMLHttpRequest();
+          if(checkbox[0].checked){ xhr.open("GET", "/update?output="+checkbox[0].id+"&state=1", true); }
+          else { xhr.open("GET", "/update?output="+checkbox[0].id+"&state=0", true); }
+          xhr.send();
+      }
+
+      console.log(arr_check);
   }, false);
   </script> 
   
@@ -497,4 +484,4 @@ void setup(){
 
 void loop() {
 
-} 
+}
