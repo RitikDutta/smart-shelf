0bee9d211fedacf7cdeec72244f9a1175b77f923
ritik dutta <ritikduttagd@gmail.com>
2022-09-18 12:03:13 +0530

    reformatting js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 arr_check = []
 document.addEventListener('keydown', (event) => {
-		arr_check = [];
+    arr_check = [];
     var name = event.key;
     var code = event.code;
     // alert(`Key pressed ${name} \n Key code Value: ${code}`);
@@ -40,56 +40,58 @@ document.addEventListener('keydown', (event) => {
 
 
 
-function animate(iter, t, arr_check, inverse){
-	console.log(arr_check)
+function animate(iter, t, arr_check, inverse) {
+    console.log(arr_check)
 
-	if (inverse == 1) {
-		arr_check.forEach(myFunction)
-	}
-	for (let i = 1; i < iter; i++) {
-	  setTimeout(function timer() {
-	    arr_check.forEach(myFunction) //  your code here
-	  if(i == iter - 1){
-			clearmyFunction()
-			console.log("Function cleared")
-		}
-	  }, i * t);
-	}
+    if (inverse == 1) {
+        arr_check.forEach(myFunction)
+    }
+    for (let i = 1; i < iter; i++) {
+        setTimeout(function timer() {
+            arr_check.forEach(myFunction) //  your code here
+            if (i == iter - 1) {
+                clearmyFunction()
+                console.log("Function cleared")
+            }
+        }, i * t);
+    }
 }
 
 
-function animate2(arr_check){
-	    for(let j = 0; j<arr_check.length-1; j++){
-			  setTimeout(function timer() {
-	    	for(let k = j+1; k <= j+1; k++) {
-	    		if (j==0) { myFunction(arr_check[0]) }
-	    		if (j==arr_check.length-2){
-	    			console.log('J IS............')
-	    			myFunction(arr_check[j+1])
-	    		}
-	    		console.log(j, k)
-	    		myFunction(arr_check[j])
-	    		myFunction(arr_check[k])
-	    	}
-			if(j == arr_check.length - 2){
-				clearmyFunction()
-				console.log("Function cleared")
-			}
-		  }, j * 200);
+function animate2(arr_check) {
+    for (let j = 0; j < arr_check.length - 1; j++) {
+        setTimeout(function timer() {
+            for (let k = j + 1; k <= j + 1; k++) {
+                if (j == 0) {
+                    myFunction(arr_check[0])
+                }
+                if (j == arr_check.length - 2) {
+                    console.log('J IS............')
+                    myFunction(arr_check[j + 1])
+                }
+                console.log(j, k)
+                myFunction(arr_check[j])
+                myFunction(arr_check[k])
+            }
+            if (j == arr_check.length - 2) {
+                clearmyFunction()
+                console.log("Function cleared")
+            }
+        }, j * 200);
     }
-	}
+}
 
 
 // animateloop(7, arr_check);
 function animateloop(number, arr_check) {
-  for (var i = 0; i < number; i++) {
-		setTimeout(function timer() {
-			// arr_check.push(1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 1, 2)
-      animate2(arr_check);
-			// arr_check = []
-	  	console.log(i)
-		}, i * 2400);
-  }
+    for (var i = 0; i < number; i++) {
+        setTimeout(function timer() {
+            // arr_check.push(1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 1, 2)
+            animate2(arr_check);
+            // arr_check = []
+            console.log(i)
+        }, i * 2400);
+    }
 }
 
 
@@ -97,10 +99,10 @@ function animateloop(number, arr_check) {
 // clearmyFunction()
 
 function clearmyFunction() {
-	for(let i = 1; i<=16; i++){
-    checkbox = document.getElementsByName(i);
-    checkbox[0].checked = false;
-	}
+    for (let i = 1; i <= 16; i++) {
+        checkbox = document.getElementsByName(i);
+        checkbox[0].checked = false;
+    }
 }
 
 
@@ -115,22 +117,21 @@ function myFunction(value) {
 
 
 const btn = document.querySelector('#sub');
-        const sb = document.querySelector('#framework')
-        btn.onclick = (event) => {
-            event.preventDefault();
-            // show the selected index
-            console.log(sb.selectedIndex);
-            if (sb.selectedIndex == 0) {
-            	arr_check = []
-            	arr_check.push(6, 7, 10, 11)
-							animate(10, 200, arr_check, 0)
-							arr_check = []
-							arr_check.push(1, 2, 3, 4, 5, 8, 9, 12, 13, 14, 15, 16)
-							animate(11, 200, arr_check, 1)
-							arr_check = []
-            }
-          else if(sb.selectedIndex == 1){
-          	arr_check.push(1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 1, 2)
-						animateloop(5, arr_check);
-          }
-        };
+const sb = document.querySelector('#framework')
+btn.onclick = (event) => {
+    event.preventDefault();
+    // show the selected index
+    console.log(sb.selectedIndex);
+    if (sb.selectedIndex == 0) {
+        arr_check = []
+        arr_check.push(6, 7, 10, 11)
+        animate(10, 200, arr_check, 0)
+        arr_check = []
+        arr_check.push(1, 2, 3, 4, 5, 8, 9, 12, 13, 14, 15, 16)
+        animate(11, 200, arr_check, 1)
+        arr_check = []
+    } else if (sb.selectedIndex == 1) {
+        arr_check.push(1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 1, 2)
+        animateloop(5, arr_check);
+    }
+};
