5c97d69d7d267249c8caf87ac29c56d2faeeb977
ritik dutta <ritikduttagd@gmail.com>
2022-09-18 13:01:35 +0530

    added anim function to esp code

--- arduino_async_webserver.ino
+++ arduino_async_webserver.ino
@@ -160,7 +160,36 @@ body {
     </style>
   </head>
   <body>
+<div class="container">
+  <div class="control-group">
+    <h1>Select boxes</h1>
+    <div class="select">
+      <select id="framework">
+        <option>First select</option>
+        <option>Option</option>
+        <option>Option</option>
+      </select>
+      <div class="select__arrow"></div>
+    </div>
+    <div class="select">
+      <select>
+        <option>Second select</option>
+        <option>Option</option>
+        <option>Option</option>
+      </select>
+      <div class="select__arrow"></div>
+    </div>
+    <div class="select">
+      <button class="button" id="sub">Submit</button>
+    </div>
+  </div>
+</div>
 
+
+
+
+
+<!-- partial:index.partial.html -->
     <fieldset class="checkbox-group">
   <legend class="checkbox-group-legend">Choose your lights</legend>
 
@@ -345,44 +374,122 @@ body {
 
 
     
-  
+
+
   <script>
-    document.addEventListener('keydown', (event) => {
+  
+  arr_check = []
+document.addEventListener('keydown', (event) => {
+    arr_check = [];
     var name = event.key;
     var code = event.code;
-    arr_check = []
+    // alert(`Key pressed ${name} \n Key code Value: ${code}`);
+    // if (name == 5) {
+    // const checkbox = document.getElementsByName(name);
+    // checkbox[0].checked = !checkbox[0].checked;
+    //NUMPAD
 
-      if (name == '1') {
+
+    if (name == '1') {
         arr_check.push(13);
-      }
-      else if (name == '3') {
+    } else if (name == '3') {
         arr_check.push(16);
-      }
-      else if (name == '7') {
+    } else if (name == '7') {
         arr_check.push(1);
-      }
-      else if (name == '9') {
+    } else if (name == '9') {
         arr_check.push(4);
-      }
-      else if (name == '2') {
+    } else if (name == '2') {
         arr_check.push(14, 15);
-      }
-      else if (name == '4') {
+    } else if (name == '4') {
         arr_check.push(5, 9);
-      }
-      else if (name == '6') {
+    } else if (name == '6') {
         arr_check.push(8, 12);
-      }
-      else if (name == '8') {
+    } else if (name == '8') {
         arr_check.push(2, 3);
-      }
-      else if (name == '5') {
-        arr_check.push(6, 7, 10, 11);       
-      }
+    } else if (name == '5') {
+        arr_check.push(6, 7, 10, 11);
+    }
 
-      arr_check.forEach(myFunction);
+    arr_check.forEach(myFunction);
 
-      function myFunction(value) {
+
+
+
+    console.log(arr_check);
+}, false);
+
+
+
+function animate(iter, t, arr_check, inverse) {
+    console.log(arr_check)
+
+    if (inverse == 1) {
+        arr_check.forEach(myFunction)
+    }
+    for (let i = 1; i < iter; i++) {
+        setTimeout(function timer() {
+            arr_check.forEach(myFunction) //  your code here
+            if (i == iter - 1) {
+                clearmyFunction()
+                console.log("Function cleared")
+            }
+        }, i * t);
+    }
+}
+
+
+function animate2(arr_check) {
+    for (let j = 0; j < arr_check.length - 1; j++) {
+        setTimeout(function timer() {
+            for (let k = j + 1; k <= j + 1; k++) {
+                if (j == 0) {
+                    myFunction(arr_check[0])
+                }
+                if (j == arr_check.length - 2) {
+                    console.log('J IS............')
+                    myFunction(arr_check[j + 1])
+                }
+                console.log(j, k)
+                myFunction(arr_check[j])
+                myFunction(arr_check[k])
+            }
+            if (j == arr_check.length - 2) {
+                clearmyFunction()
+                console.log("Function cleared")
+            }
+        }, j * 200);
+    }
+}
+
+
+// animateloop(7, arr_check);
+function animateloop(number, arr_check) {
+    for (var i = 0; i < number; i++) {
+        setTimeout(function timer() {
+            // arr_check.push(1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 1, 2)
+            animate2(arr_check);
+            // arr_check = []
+            console.log(i)
+        }, i * 2400);
+    }
+}
+
+
+
+// clearmyFunction()
+
+function clearmyFunction() {
+    for (let i = 1; i <= 16; i++) {
+        checkbox = document.getElementsByName(i);
+        checkbox[0].checked = false;
+    }
+}
+
+
+// arr_check.push(2, 3, 4)
+// animate(10, 200, arr_check)
+
+ function myFunction(value) {
         checkbox = document.getElementsByName(value);
         checkbox[0].checked = !checkbox[0].checked;
         var xhr = new XMLHttpRequest();
@@ -391,9 +498,31 @@ body {
           xhr.send();
       }
 
-      console.log(arr_check);
-  }, false);
-  </script> 
+
+
+const btn = document.querySelector('#sub');
+const sb = document.querySelector('#framework')
+btn.onclick = (event) => {
+    event.preventDefault();
+    // show the selected index
+    console.log(sb.selectedIndex);
+    if (sb.selectedIndex == 0) {
+        arr_check = []
+        arr_check.push(6, 7, 10, 11)
+        animate(10, 200, arr_check, 0)
+        arr_check = []
+        arr_check.push(1, 2, 3, 4, 5, 8, 9, 12, 13, 14, 15, 16)
+        animate(11, 200, arr_check, 1)
+        arr_check = []
+    } else if (sb.selectedIndex == 1) {
+        arr_check.push(1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 1, 2)
+        animateloop(5, arr_check);
+    }
+};
+  
+  
+  
+  </script>
   
     <script>function toggleCheckbox(element) {
       var xhr = new XMLHttpRequest();
@@ -433,7 +562,6 @@ void setup(){
   pinMode(21, OUTPUT);
   pinMode(22, OUTPUT);
   pinMode(23, OUTPUT);
-
   pinMode(13, OUTPUT);
   pinMode(14, OUTPUT);
   pinMode(27, OUTPUT);
@@ -441,6 +569,7 @@ void setup(){
   pinMode(25, OUTPUT);
   pinMode(33, OUTPUT);
   pinMode(32, OUTPUT);
+
   
   // Connect to Wi-Fi
   WiFi.begin(ssid, password);
@@ -465,7 +594,7 @@ void setup(){
     if (request->hasParam(PARAM_INPUT_1) && request->hasParam(PARAM_INPUT_2)) {
       inputMessage1 = request->getParam(PARAM_INPUT_1)->value();
       inputMessage2 = request->getParam(PARAM_INPUT_2)->value();
-      digitalWrite(inputMessage1.toInt(), inputMessage2.toInt());
+      digitalWrite(inputMessage1.toInt(), !inputMessage2.toInt());
     }
     else {
       inputMessage1 = "No message sent";
